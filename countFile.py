8afcd1d17036fa7e0183a85079a9f40083069020
Jack xie <xieyixiao1234x@gmail.com>
2019-04-04 23:47:37 +0800

    v1.0

--- countFile.py
+++ countFile.py
@@ -4,7 +4,7 @@ result = []
 
 
 def get_all(cwd):
-    get_dir = os.listdir(cwd)  # 遍历当前目录，获取文件列表
+    get_dir = os.listdir(cwd) # 遍历当前目录，获取文件列表
     for i in get_dir:
         sub_dir = os.path.join(cwd, i)  # 把第一步获取的文件加入路径
         if os.path.isdir(sub_dir):  # 如果当前仍然是文件夹，递归调用
@@ -12,9 +12,10 @@ def get_all(cwd):
         else:
             ax = os.path.basename(sub_dir)  # 如果当前路径不是文件夹，则把文件名放入列表
             result.append(ax)
-            print(len(result))  # 对列表计数
+    # print(len(result))  # 对列表计数
 
 
 if __name__ == "__main__":
     cur_path = os.getcwd()  # 当前目录
     get_all(cur_path)
+    print(len(result))  # 对列表计数
